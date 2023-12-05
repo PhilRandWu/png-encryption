f1b5bdae056265647b39278a327535da9659bc77
Wenbo.Wu <3378814052@qq.com>
2023-12-05 17:14:44 +0800

    feat: args parse

---
+++ src/args.rs
@@ -0,0 +1,39 @@
+#[derive(Debug, Parser)]
+#[clap(name = "pngEnc")]
+pub struct Cli {
+    #[clap(subcommand)]
+    pub subcommand: PngMeArgs,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum PngMeArgs {
+    Encode(EncodeArgs),
+    Decode(DecodeArgs),
+    Remove(RemoveArgs),
+    Print(PrintArgs),
+}
+
+#[derive(Debug, Args)]
+pub struct EncodeArgs {
+    pub file_path: PathBuf,
+    pub chunk_type: String,
+    pub message: String,
+    pub output: Option<PathBuf>,
+}
+
+#[derive(Debug, Args)]
+pub struct DecodeArgs {
+    pub file_path: PathBuf,
+    pub chunk_type: String,
+}
+
+#[derive(Debug, Args)]
+pub struct RemoveArgs {
+    pub file_path: PathBuf,
+    pub chunk_type: String,
+}
+
+#[derive(Debug, Args)]
+pub struct PrintArgs {
+    pub file_path: PathBuf,
+}
